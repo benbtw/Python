ea8bf848fd742f91f1558ab2ed87db9903094034
benbtw <58542595+benbtw@users.noreply.github.com>
2023-07-21 21:35:23 -0500

    tictactoechange

--- stuff/tic_tac_toe/game.py
+++ stuff/tic_tac_toe/game.py
@@ -1,24 +1,40 @@
-class TicTacToe:
-    def __init__(self):
-        self.board = [' ' for _ in range(9)]
-        self.current_winner = None
-        
-    def print_board(self):
-        for row in [self.board[i*3:(i+1)*3] for i in range(3)]:
-            print('| ' + ' | '.join(row) + ' |')
-            
-    @staticmethod
-    def print_board_nums():
-        number_board = [[str(i) for i in range(j*3, (j+1)*3)] for j in range (3)]
-        for row in number_board:
-            print('| ' + ' | '.join(row) + ' |')
-    
-    def available_moves(self):
-        return [i for i, spot in enumerate(self.board) if spot == ' ']
-    
-def play(game, x_player, o_player, print_game=True):
-    if print_game:
-        game.print_board_nums()
-    
-    letter = 'X'
-    
+import os
+
+@staticmethod
+def print_board():
+    count = 0
+    board = ['|', '  ', '|', '  ', '|', '  ', '|',
+         '|','--','|','--','|','--','|',
+         '|', '  ', '|', '  ', '|', '  ', '|',
+         '|','--','|','--','|','--','|',
+         '|', '  ', '|', '  ', '|', '  ', '|']
+    for i in board:
+        if i == '|':
+            count += 1
+        if count == 4 or count == 8 or count == 12 or count == 16:
+            print(i,end='\n')
+        else:
+            print(i, end='')
+
+@staticmethod
+def play_game():
+    computer = ''
+    player = input('Would you like to be X or O: ').upper()
+    while True:
+        os.system('cls')
+        if player == "X" or player == "O":
+            break
+        else:
+            print('Error, enter X or O', end='\n')
+            player = input('Would you like to be X or O: ').upper()
+
+    if player == 'X':
+        computer = 'O'
+    elif player == 'O':
+        computer = 'X'
+
+    print(f'You have chosen {player}, good luck.')
+                          
+
+os.system('cls')
+play_game()
