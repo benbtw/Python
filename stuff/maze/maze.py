88a1b0914130fa6a4ba6bdcb96890ea616718084
benbtw <58542595+benbtw@users.noreply.github.com>
2023-07-23 22:11:22 -0500

    final maze

--- stuff/maze/maze.py
+++ stuff/maze/maze.py
@@ -1,26 +1,62 @@
 import os
 
-"""
-create small basic maze
-    - function for player movement with collision detection
-    - function for printing/updating the map
-    - funciton for winning
-    - potential function for welcome screen
-"""
 
 board = [
-    '|', '‾', '‾', '‾','‾', '‾' ,'‾','‾', '‾', '‾','‾', '‾', '‾', '|',
-    '|', ' ', ' ', ' ',' ', ' ', ' ',' ', ' ', ' ',' ', ' ', ' ', '|',
-    '|', ' ', ' ', ' ',' ', ' ', ' ',' ', ' ', ' ',' ', ' ', ' ', '|',
-    '|', ' ', ' ', ' ',' ', ' ', ' ',' ', ' ', ' ',' ', ' ', ' ', '|',
-    '|', ' ', ' ', ' ',' ', ' ', ' ',' ', ' ', ' ',' ', ' ', ' ', '|',
-    '|', ' ', ' ', ' ',' ', ' ', ' ',' ', ' ', ' ',' ', ' ', ' ', '|',
-    '|', '_', '_', '_','_', '_','_','_', '_', '_','_', '_', '_', '|'
+    ['#','#','#','#','#','#','#','#','#','#','#'],
+    ['#',' ',' ',' ',' ',' ','#',' ',' ',' ','#'],
+    ['#',' ','#','#','#','#','#','#','#',' ','#'],
+    ['#',' ',' ',' ','#',' ','#',' ',' ',' ','#'],
+    ['#','#','#',' ','#',' ','#',' ','#','#','#'],
+    ['#',' ',' ',' ',' ',' ','#',' ',' ',' ','#'],
+    ['#','#','#',' ','#','#','#','#','#',' ','#'],
+    ['#',' ','#',' ','#',' ',' ',' ',' ',' ','#'],
+    ['#',' ','#',' ','#',' ','#',' ','#','#','#'],
+    ['#',' ',' ',' ',' ',' ','#',' ',' ',' ','#'],
+    ['#','#','#','#','#','#','#','#','#','F','#']
 ]
 
-count = 0
-for i in board:
-    if count % 14 == 0:
-        print()
-    print(i, end='')
-    count += 1
+
+
+player = 'P'
+class Maze:
+
+    player_x = 1
+    player_y = 1
+    running = True
+
+
+    def print_maze(self):
+        os.system('cls')
+        board[self.player_y][self.player_x] = player
+        print(self.player_y, self.player_x)
+        for row in board:
+            print(*row, sep="")
+
+
+    def player_movement(self, player):
+        move = input("Move(WASD): ").upper()
+        if (move == 'W' and board[self.player_y-1][self.player_x] != '#'):
+            board[self.player_y][self.player_x] = ' '
+            self.player_y -= 1
+        elif (move == 'S' and board[self.player_y+1][self.player_x] != '#'):
+            if board[self.player_y+1][self.player_x] == 'F':
+                print("Congrats, you beat the maze!")
+                self.running = False
+            board[self.player_y][self.player_x] = ' '
+            self.player_y += 1
+        elif (move == 'A' and board[self.player_y][self.player_x-1] != '#'):
+            board[self.player_y][self.player_x] = ' '
+            self.player_x -= 1
+        elif (move == 'D' and board[self.player_y][self.player_x+1] != '#'):
+            board[self.player_y][self.player_x] = ' '
+            self.player_x += 1
+
+
+
+
+m = Maze()
+
+
+while m.running:
+    m.print_maze()
+    m.player_movement(player)
